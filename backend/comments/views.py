84a4e210a82db9ca31d8cd1357f142b945655a07
AenEnlil <morphinegermanika@gmail.com>
2025-05-01 15:26:03 +0300

    CommentViewSet now inherits from GenericViewset. Added get_queryset method

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,19 +1,28 @@
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ViewSet, GenericViewSet
 
 from .models import Comment
 from .serializers import CommentSerializer
 
 
-class CommentViewSet(ViewSet):
+class CommentViewSet(GenericViewSet):
 
     queryset = Comment.objects.all().order_by('-created_at')
     serializer = CommentSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action == 'list':
+            queryset = queryset.filter(parent=None)
+        elif self.action == 'get_replies':
+            queryset = queryset.filter(parent_id=self.kwargs.get('pk'))
+        return queryset
+
     def list(self, request):
-        serializer = self.serializer(self.queryset.filter(parent=None), many=True)
+        queryset = self.get_queryset()
+        serializer = self.serializer(queryset, many=True)
         return Response(serializer.data)
 
     def create(self, request):
@@ -23,11 +32,12 @@ class CommentViewSet(ViewSet):
         return Response(serializer.data)
 
     def retrieve(self, request, *args, **kwargs):
-        instance = get_object_or_404(self.queryset, id=kwargs.get('pk', None))
+        instance = self.get_object()
         serializer = self.serializer(instance)
         return Response(serializer.data)
 
     @action(methods=['get'], detail=False, url_path='(?P<pk>[0-9]+)/replies')
-    def get_replies(self, request, pk: int, *args, **kwargs):
-        serializer = self.serializer(self.queryset.filter(parent_id=pk), many=True)
+    def get_replies(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.serializer(queryset, many=True)
         return Response(serializer.data)
