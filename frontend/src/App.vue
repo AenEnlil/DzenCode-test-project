7197bf5a581c0a3553e357c761b53b40e74303a3
AenEnlil <morphinegermanika@gmail.com>
2025-05-10 17:06:07 +0300

    [frontend]: added header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,17 @@
 
 <template>
  <div>
+  <SiteHeader />
   <router-view />
  </div>
 </template>
+
+<script>
+import SiteHeader from '@/components/Header.vue'
+
+export default {
+    components: {
+        SiteHeader
+    }
+}
+</script>
