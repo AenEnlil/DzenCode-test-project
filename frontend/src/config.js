4629d6ecbf993ec727c74619486feba97db415c5
AenEnlil <morphinegermanika@gmail.com>
2025-05-12 01:02:36 +0300

    [deploy]: added nginx proxy to backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8000/api/v1';
+export const API_BASE_URL = import.meta.env.VITE_BACKEND_API_URL
 export const WS_BASE_URL = 'ws://localhost:8000'
 export const PAGE_SIZE = 25
