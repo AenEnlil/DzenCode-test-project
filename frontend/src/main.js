3644cfd0aa5e36e30775d5aae5274740060b81dc
AenEnlil <morphinegermanika@gmail.com>
2025-05-12 13:40:38 +0300

    [deploy]: added websockets forward proxy

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,5 +6,5 @@ import router from './router'
 import { connectWS } from '@/services/websocket.js'
 import {WS_BASE_URL} from '@/config.js'
 
-connectWS(`${WS_BASE_URL}/ws/comments/`);
+connectWS(`${WS_BASE_URL}/comments/`);
 createApp(App).use(router).mount('#app')
